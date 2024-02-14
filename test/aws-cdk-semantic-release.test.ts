2275670900a6c44fcf49a3dd661ff418216dbfbb
Brandon Kindred <brandon.kindred@gmail.com>
2024-02-13 21:54:31 -0500

    feat(cdk): init cdk

---
+++ test/aws-cdk-semantic-release.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkSemanticRelease from '../lib/aws-cdk-semantic-release-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-semantic-release-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkSemanticRelease.AwsCdkSemanticReleaseStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
